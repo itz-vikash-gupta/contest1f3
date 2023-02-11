011eb241973d44320f26b9648e00f156265b2c4d
itz-vikash-gupta <viku9211gupta@gmail.com>
2023-02-11 22:35:54 +0530

    ok

---
+++ index.js
@@ -0,0 +1,42 @@
+// api url
+let api_url = "https://dummyjson.com/products";
+
+function getapi(url) {
+  fetch(url).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Something went wrong");
+    })
+    .then((responseJson) => {
+      show(responseJson);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getapi(api_url);
+
+function show(data) {
+  let tab = "";
+ // Loop to access all rows
+ for (let r of data.products) {
+    tab += `
+        <div id="mainDiv" style="border:1px solid black;">
+        <div id="productImg">
+            
+            <img src = ${r.thumbnail} height='280px' width='320px'>
+        </div>
+        <div id="productDetails" style="padding-left:3%">
+        Name : ${r.title} <br>
+        Id : ${r.id} <br>
+        Price : ${r.price} <br>
+        Discount % : ${r.discountPercentage}<br>
+        Stock : ${r.stock}
+        </div>
+           </div>`;
+  }
+  // Setting innerHTML as tab variable
+  document.getElementById("product").innerHTML=tab;
+}
